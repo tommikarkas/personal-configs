de773e6b45b899efffc1da598b4c057ebc336666
Tommi Kärkäs <tommi.karkas@gofore.com>
2019-12-15 11:01:22 +0200

    Added gitconfig to personal configs + symlink generation

--- scripts/create-symlinks.sh
+++ scripts/create-symlinks.sh
@@ -8,3 +8,4 @@ ln -fs "${TARGET_DIR}.bashrc" ~/.bashrc
 ln -fs "${TARGET_DIR}.bash_aliases" ~/.bash_aliases 
 ln -fs "${TARGET_DIR}.vimrc" ~/.vimrc 
 ln -fs "${TARGET_DIR}.env_vars" ~/.env_vars 
+ln -fs "${TARGET_DIR}.gitconfig" ~/.gitconfig 

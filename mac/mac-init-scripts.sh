0c8b6305b1b4f8034fae1ce5c4b2acd5eee21815
Tommi Kärkäs <tommi.karkas@gofore.com>
2020-09-11 11:30:43 +0300

    Update, Mac specific initialization tuned to MacOS Catalina

--- mac/mac-init-scripts.sh
+++ mac/mac-init-scripts.sh
@@ -7,18 +7,29 @@ set -x
 
 # Config variables
 BREW_PACKAGES="git bash bash-completion jenv thefuck"
+PROJECT_DIR="/Projects/Personal/personal-configs/mac/"
+TARGET_DIR="${HOME}${PROJECT_DIR}"
 
-
-main () {
+installBrewPackages () {
     brew install ${BREW_PACKAGES}
+}
+
+setBashProfile () {
+    ln -fs "${TARGET_DIR}.bash_profile" ~/.bash_profile 
+}
+
+changeBackToBash () {
+    chsh -s /bin/bash
 }
 
 if brew --version > /dev/null; then
   # The package is installed
-  main
+  installBrewPackages
 else
   # The package is not installed
   echo "Install brew and try running the script again"
 fi
 
+setBashProfile
+changeBackToBash
 
